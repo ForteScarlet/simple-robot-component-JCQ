9c7719f215675494a44baf1e493afdb015efc980
ForteScarlet <ForteScarlet@163.com>
2020-07-04 23:20:38 +0800

    update copyright

--- src/main/java/com/simplerobot/component/jcq/JCQMain.kt
+++ src/main/java/com/simplerobot/component/jcq/JCQMain.kt
@@ -1,3 +1,19 @@
+/*
+ * Copyright (c) 2020. ForteScarlet All rights reserved.
+ * Project  component-jcq (Code other than JCQ)
+ * File     JCQMain.kt (Code other than JCQ)
+ *
+ * You can contact the author through the following channels:
+ * github https://github.com/ForteScarlet
+ * gitee  https://gitee.com/ForteScarlet
+ * email  ForteScarlet@163.com
+ * QQ     1149159218
+ *
+ * JCQ's code is copyrighted by JCQ
+ * you can see at: https://github.com/meowya/jcq-coolq
+ *
+ */
+
 @file:Suppress("MemberVisibilityCanBePrivate")
 
 package com.simplerobot.component.jcq
